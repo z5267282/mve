5aae5ec6e7043cce8d5da69fefd229dc01ff74bd
Sunny Windows <z5267282@student.unsw.edu>
2022-05-24 02:57:32 +1000

    Need to fix strange bug

--- src/config.py
+++ src/config.py
@@ -1,1 +1,8 @@
-SOURCE = ['D:', 'Videos', 'Team Fortress 2']
+SOURCE_PATH  = ['D:', 'Videos', 'Batches', '1']
+RENAMES_PATH = ['D:', 'Videos', 'Renames']
+
+EDITS     = 'edits.json'
+RENAMES   = 'renames.json'
+DELETIONS = 'deletions.json'
+
+REMAINING = 'remaining.json'
