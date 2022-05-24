af97715d3ce5dc8a3e2324df2f6790aa15c1cacb
Sunny Windows <z5267282@student.unsw.edu>
2022-05-25 02:23:22 +1000

    Forgot to add files from last commit

--- src/generate.py
+++ src/generate.py
@@ -1,8 +1,25 @@
 import os
+import sys
 
+import config as cfg
 import helper as helper
 
-
+def files_remaining():
+    remaining_file = cfg.REMAINING
+    return os.path.exists(remaining_file) and bool(
+        helper.load_from_json(remaining_file)
+    )
 
 def main():
-    cfg = helper.load_config()
+    if files_remaining():
+        print(
+            "couldn't generate list of remaining files; there are files left to be edited",
+            file=sys.stderr
+        )
+        sys.exit(1)
+
+    abs_file_names = helper.ls_abs_path(cfg.SOURCE_PATH)
+    helper.write_to_json(abs_file_names, cfg.REMAINING)
+
+if __name__ == '__main__':
+    main()
--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,4 @@
+import json
 import moviepy as mvp
 import os
 
@@ -7,26 +8,22 @@ import config as cfg
     FILE RELATED
 """
 
-def my_ls(paths_list):
-    print(paths_list)
-    print('fish')
-    path = os.path.join(*paths_list)
-    return os.listdir(path)
-
-def load_file(paths_list, file_name):
+def get_abs_path(paths_list, file_name):
     paths = paths_list + [file_name]
     return os.path.join(*paths)
 
-def load_source_file(file_path):
-    return load_file(cfg.SOURCE_PATH, file_path)
+def ls(paths_list):
+    path = os.path.join(*paths_list)
+    return os.listdir(path)
+
+def ls_abs_path(paths_list):
+    return [get_abs_path(paths_list, file_name) for file_name in ls(paths_list)]
 
 def get_earliest_file(paths_list):
-    files = my_ls(paths_list)
-    print(len(files))
+    files = ls_abs_path(paths_list)
     return sorted(
         files, 
-        key=lambda file_path: os.path.getctime(load_source_file(file_path)), 
-        reverse=True
+        key=os.path.getctime
     )[0] if files else None
 
 """
@@ -43,3 +40,16 @@ def handle_clip(src_full_path, success_handler, start, end):
             error = str(e)
 
     return error
+
+"""
+    JSON
+"""
+
+def write_to_json(item, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(item, f, indent=cfg.JSON_INDENT)
+
+def load_from_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
