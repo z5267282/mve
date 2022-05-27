6c9239eb88e258d3e65ae5952de64061165456d1
Sunny Windows <z5267282@student.unsw.edu>
2022-05-28 02:36:19 +1000

    Realising that subclip() cannot check for duration errors

--- src/helpers/file_system.py
+++ src/helpers/file_system.py
@@ -3,3 +3,7 @@ import os
 def get_joined_path(paths_list, file_name):
     paths = paths_list + [file_name]
     return os.path.join(*paths)
+
+def ls(paths_list):
+    path = os.path.join(*paths_list)
+    return os.listdir(path)
