b2f2d09a888383c49153eb61a6f882312915fa80
Sunny Windows <z5267282@student.unsw.edu>
2022-05-31 00:28:06 +1000

    Switching over to a more simple file structure

--- src/constants/error_numbers.py
+++ src/constants/error_numbers.py
@@ -7,8 +7,9 @@ MISSING_REMAINING = 3
 
 # config path folders
 NO_SOURCE_FOLDER  = 4 
-NO_RENAMES_FOLDER = 5 
+NO_EDITS_FOLDER   = 5
+NO_RENAMES_FOLDER = 6 
 
 # queue
-NO_QUEUE    = 6 
-EMPTY_QUEUE = 7 
+NO_QUEUE    = 7 
+EMPTY_QUEUE = 8 
