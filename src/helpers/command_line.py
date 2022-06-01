41f92dd1cedf05753d13b4536e1734e055104fd9
Sunny Chen <z5267282@student.unsw.edu.au>
2022-06-01 16:49:10 +1000

    Minor changes to the helper files

--- src/helpers/command_line.py
+++ src/helpers/command_line.py
@@ -1,11 +1,11 @@
 import os.path
 import sys
 
-import constants.commands as commands 
-import constants.error_numbers as error_numbers
+import constants.error as err 
+import util
 
 def command_line_error(argv):
     program_name, args = argv[0], argv[1:]
-    if len(args) != commands.COMMAND_LINE_LENGTH:
-        print(f'usage: {os.path.basename(program_name)}')
-        sys.exit(error_numbers.BAD_COMMAND_LINE_ARGS)
+    if args:
+        util.stderr_print(f'usage: {os.path.basename(program_name)}')
+        sys.exit(err.BAD_COMMAND_LINE_ARGS)
