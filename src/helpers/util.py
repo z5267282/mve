c32e90303ac38a7c59398dc4c9938d1a578eb76f
Sunny Chen <z5267282@student.unsw.edu.au>
2022-06-01 13:56:21 +1000

    Nearly finished the viewer

--- src/helpers/util.py
+++ src/helpers/util.py
@@ -2,6 +2,8 @@ import datetime as dt
 import json
 import sys
 
+import constants.file_structure as fst
+
 def write_to_json(item, file_path):
     with open(file_path, 'w') as f:
         json.dump(item, f, indent=4)
@@ -10,6 +12,9 @@ def read_from_json(file_path):
     with open(file_path, 'r') as f:
         data = json.load(f)
     return data
+
+def load_treatments():
+    return read_from_json(fst.REMAINING) 
 
 
 def get_timestamp():
--- src/viewer.py
+++ src/viewer.py
@@ -2,11 +2,15 @@ import os
 import re
 import sys
 
-import constants.cmd as cmd
-import constants.err as err
-import constants.fst as fst
+import config as cfg
+
+import constants.commands as cmd
+import constants.error as err
+import constants.file_structure as fst
+import constants.treatment_format as trf
 
 import helpers.check_and_exit_if as check_and_exit_if
+import helpers.files as files
 import helpers.util as util
 
 def no_remaining():
@@ -20,35 +24,138 @@ def run_checks():
     check_and_exit_if.no_source_folder()
 
 
+def print_time_format(name, form):
+    print(f'the {name} time must be in the form {form}')
+
+def parse_timestamp(timestamp):
+    return timestamp.replace('-', ':') if re.fullmatch(r'[0-9]?[0-9]-[0-9]?[0-9]', timestamp) else None
+
+def print_name_format():
+    print('the name can only contain upper and lowercase letters, digits and spacebars')
+
 def correct_name_format(name):
     return re.fullmatch(r'[a-zA-Z ]', name)
 
-def log_edit(name, edit_name, times, edits):
-    pass
+def bad_name_format(name):
+    if not correct_name_format(name):
+        print_name_format()
+        return True
 
-def do_end(tokens, edits):
-    if len(tokens) != 2:
+    return False
+
+
+def tokenise(raw_tokens, splits):
+    return raw_tokens.split(' ', splits)
+
+def parse_tokens(raw_tokens, command):
+    n_tokens = cmd.NUM_TOKENS[command]
+    tokens = tokenise(raw_tokens, n_tokens - 1)
+    return tokens if len(tokens) == tokens else []
+
+
+def log_edit(name, edit_name, times, edits):
+    edits[name] = {
+        trf.EDIT_NAME  : edit_name,
+        trf.EDIT_TIMES : times
+    }
+
+def do_end(name, raw_tokens, edits):
+    tokens = parse_tokens(raw_tokens, cmd.END)
+    if not tokens:
         print('[e]nd | [ time ] [ name ]')
         return
 
     raw_time, edit_name = tokens
     time = None
-    if re.fullmatch(r'[0-9]+', raw_time):
+    if re.fullmatch(r'-?[0-9]+', raw_time):
         time = raw_time
-    elif re.fullmatch(r'[0-9]?[0-9]-[0-9]?[0-9]'):
-        time = raw_time.replace('-', ':')
-
+    else:
+        time = parse_timestamp(raw_time)
     if time is None:
-        print('the time is in the form [ 1 integer | timestamp in form <min-sec>) ]')
+        print_time_format('', '[ integer | timestamp in form <min-sec>) ]')
         return
 
-    if not correct_name_format(edit_name):
-        print('the name can only contain upper and lowercase letters, digits and spacebars')
+    if bad_name_format(edit_name):
         return
+
+    log_edit(name, edit_name, [time], edits)
+
+def do_middle(name, raw_tokens, edits):
+    tokens = parse_tokens(raw_tokens, cmd.MIDDLE)
+    if not tokens:
+        print('[m]iddle | [ start ] [ end ] [ name ]')
+        return
+
+    start, end, edit_name = tokens
+    times = []
+    for value, description in enumerate([start, end], ['start', 'end']):
+        if re.fullmatch(r'[0-9]+', value):
+            times.append(value)
+            continue
+
+        time = parse_timestamp(value)
+        if time is None:
+            print_time_format(description, '[ natural number | timestamp in form <min-sec>) ]')
+            return
+
+        times.append(time)
+    
+    if bad_name_format(edit_name):
+        return
+
+    log_edit(name, edit_name, times, edits)
+
+def log_rename(name, new_name, renames):
+    renames[name] = new_name
+
+def do_rename(name, raw_tokens, renames):
+    tokens = parse_tokens(raw_tokens, cmd.RENAME)
+    if not tokens:
+        print('[r]ename | [ name ]')
+        return
+    
+    new_name, = tokens
+    if bad_name_format(new_name):
+        return
+    
+    log_rename(name, new_name, renames)
+
+def log_delete(name, deletions):
+    deletions.append(name)
+
+def view_video(name):
+    joined_path = files.get_joined_path(cfg.SOURCE, name)
+    # TODO: load video
+    print(joined_path)
+
+def run_loop(remaining, edits, renames, deletions):
+    while remaining:
+        name = remaining[0]
+        view_video(name)
+
+        args = input(f'{name}: ')
+        command = args.split(' ', 1)
+        match command:
+            case cmd.QUIT:
+                break
+            case cmd.CONTINUE:
+                remaining.insert(0, name)
+                break
+            case cmd.HELP:
+                print(cmd.MESSAGE)
+                break
+            
+
+
+def log_to_file():
+    pass
 
 
 def main():
     run_checks()
+
+    run_loop()
+    log_to_file()
 
 if __name__ == '__main__':
     main()
