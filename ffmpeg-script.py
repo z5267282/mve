1c45ae5956464a3d01f9e0e5805270af1a5778e2
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-26 02:04:27 +1000

    minor progress on ffmpeg

--- ffmpeg-script.py
+++ ffmpeg-script.py
@@ -3,7 +3,8 @@ import subprocess
 name = 'jisoo.mp4'
 # start, end  = '00:00:02', '00:00:08'
 # start, end = '60', '63'
-start, end = '00:00:57', '00:01:09'
+# start, end = '00:00:57', '00:01:09'
+start, end = '00:03', '00:10'
 out = 'dog.mp4'
 
 args = ['ffmpeg', '-y', '-i', name, '-ss', start, '-to', end, out]
