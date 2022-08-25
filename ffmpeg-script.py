81c9006ae89046351092e0b98f75ae0c74a51fbd
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-26 02:21:55 +1000

    new editor should work

--- ffmpeg-script.py
+++ ffmpeg-script.py
@@ -7,10 +7,11 @@ name = 'jisoo.mp4'
 start, end = '00:03', '00:10'
 out = 'dog.mp4'
 
-args = ['ffmpeg', '-y', '-i', name, '-ss', start, '-to', end, out]
+# args = ['ffmpeg', '-y', '-i', name, '-ss', start, '-to', end, out]
 
 # last n seconds
 # args = ['ffmpeg', '-y', '-sseof', '-10', '-i', name,  out]
 
-subprocess.run(args)
+args = ['ffmpeg', '-y', '-i', name, '-ss', '3:43', out]
 
+subprocess.run(args)
