96365fe348844d4ec4529e182d6f13b16331ad09
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-25 20:40:23 +1000

    Trying to use negative times

--- ffmpeg-script.py
+++ ffmpeg-script.py
@@ -1,8 +1,7 @@
 import subprocess
 
 name = 'Cirstea vs Watson.mp4'
-start = '00:00:02'
-end = '00:00:08'
+start, end  = '00:00:02', '00:00:08'
 out = 'fish.mp4'
 
 args = ['ffmpeg', '-ss', start, '-i', name, '-to', end, '-codec', 'copy', '-copyts', '-y', out]
