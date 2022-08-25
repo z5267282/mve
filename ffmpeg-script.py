d830132978adddef721f80742dcad1faa2bd4b8c
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-26 01:54:42 +1000

    giving up on codec copy

--- ffmpeg-script.py
+++ ffmpeg-script.py
@@ -1,11 +1,15 @@
 import subprocess
 
-name = 'Cirstea vs Watson.mp4'
+name = 'jisoo.mp4'
 # start, end  = '00:00:02', '00:00:08'
-start, end = '00:00:00', '63'
-out = 'fish.mp4'
+# start, end = '60', '63'
+start, end = '00:00:57', '00:01:09'
+out = 'dog.mp4'
 
-args = ['ffmpeg', '-ss', start, '-i', name, '-to', end, '-codec', 'copy', '-copyts', '-y', out]
+args = ['ffmpeg', '-y', '-i', name, '-ss', start, '-to', end, out]
+
+# last n seconds
+# args = ['ffmpeg', '-y', '-sseof', '-10', '-i', name,  out]
 
 subprocess.run(args)
 
