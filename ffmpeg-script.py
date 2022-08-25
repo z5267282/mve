4458ea07697027522736dc47f3e37ab3b74196b7
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-25 23:06:51 +1000

    seconds not seem to be working

--- ffmpeg-script.py
+++ ffmpeg-script.py
@@ -1,7 +1,8 @@
 import subprocess
 
 name = 'Cirstea vs Watson.mp4'
-start, end  = '00:00:02', '00:00:08'
+# start, end  = '00:00:02', '00:00:08'
+start, end = '00:00:00', '63'
 out = 'fish.mp4'
 
 args = ['ffmpeg', '-ss', start, '-i', name, '-to', end, '-codec', 'copy', '-copyts', '-y', out]
