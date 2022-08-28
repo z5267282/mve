ce0205078abfccd76fbc93fd63a59537a7e60164
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-28 23:55:42 +1000

    timestamp checking

--- src/treater.py
+++ src/treater.py
@@ -1,4 +1,5 @@
 import concurrent.futures
+from ntpath import join
 import os
 import subprocess
 import sys
@@ -74,6 +75,40 @@ def handle_error(errors, remaining, name, message, command, data):
 def add_suffix(joined_path):
     return joined_path + video_editing.SUFFIX
 
+def get_duration(joined_src_path):
+    args = [
+        'ffprobe',
+        '-i',
+        joined_src_path,
+        '-v',
+        'quiet',
+        '-show_entries',
+        'format=duration',
+        '-hide_banner',
+        '-of',
+        'default=noprint_wrappers=1:nokey=1'
+    ]
+    result = subprocess.run(args, capture_output=True, text=True)
+
+    return int(float(result.stdout))
+
+def check_timestamp(timestamp, duration):
+    return sum(
+        int(t) * (60 ** i)
+            for t, i in enumerate(reversed(timestamp.split(':')), start=1)
+    ) <= duration
+
+def check_good_time(joined_src_path, time):
+    duration = get_duration(joined_src_path)
+
+    if time.startswith('-'):
+        return int(time[1:]) <= duration 
+
+    if ':' in time:
+        return check_timestamp(time)
+
+    return int(time) <= duration
+
 def edit_video(joined_src_path, joined_dst_path, start, end=None):
     args = ['ffmpeg', '-y']
     source = ['-i', joined_src_path]
@@ -93,6 +128,9 @@ def edit_one(edit):
     )
 
     times = edit[trf.EDIT_TIMES]
+    for t in times:
+        if not check_good_time(t):
+            raise ValueError(f'invalid time: {t}')
     edit_video(joined_src_path, joined_dst_path, *times)
         
 def edit_all(edits, remaining, errors):
