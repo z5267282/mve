bb75c5dea7f49252dae611d6fb01b8dc55c0df73
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-29 02:07:08 +1000

    Getting rid of unused constants

--- src/config.py
+++ src/config.py
@@ -2,7 +2,3 @@
 SOURCE      = ['D:', '\\', 'Videos', 'Team Fortress 2']
 RENAMES     = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Renames']
 DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits']
-
-# multi threading and processing
-NUM_THREADS   = 4
-NUM_PROCESSES = 4
