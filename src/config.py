052c24e9dac2c8964d10366871477b49537071b5
Sunny Chen <z5267282@student.unsw.edu.au>
2022-08-29 22:23:50 +1000

    Realised the multiprocessing constants were needed

--- src/config.py
+++ src/config.py
@@ -2,3 +2,7 @@
 SOURCE      = ['D:', '\\', 'Videos', 'Team Fortress 2']
 RENAMES     = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Renames']
 DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits']
+
+# multi threading and processing
+NUM_THREADS   = 4
+NUM_PROCESSES = 4 
