647e700ff0d8e2050abcce5c71b1268a1c9feb7c
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-03 01:36:50 +1000

    fixing constants in treatment format

--- src/constants/treatment_format.py
+++ src/constants/treatment_format.py
@@ -1,9 +1,6 @@
 # keys within a treatment
 
-from config import DESTINATION
-
-
-EDITS     = 'edits'
+EDITS         = 'edits'
 # keys within edits
 EDIT_ORIGINAL = 'original'
 EDIT_NAME     = 'new name'
