6ec820581399f009989eec8f4179bdf82e497a74
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-05 02:32:06 +1000

    reordering treater

--- src/treater.py
+++ src/treater.py
@@ -16,6 +16,31 @@ import helpers.check_and_exit_if as check_and_exit_if
 import helpers.files as files
 import helpers.util as util
 
+
+def main():
+    run_checks()
+
+    remaining, errors = util.load_remaining(), list()
+    current_file = dequeue()
+    joined_current_file = files.get_joined_path(fst.QUEUE, current_file)
+    treat_all(joined_current_file, remaining, errors)
+    update_history(current_file, joined_current_file)
+
+    if errors:
+        handle_errors(remaining, errors)
+
+    util.exit_success('successfully treated all files')
+
+
+def run_checks():
+    check_and_exit_if.bad_args(sys.argv)
+    check_and_exit_if.no_queue()
+    check_empty_queue()
+    check_and_exit_if.no_source_folder()
+    no_renames()
+    no_history()
+    no_errors()
+
 def check_empty_queue():
     if not files.ls(fst.QUEUE):
         print(f"there are no files queued in folder '{fst.QUEUE}'")
@@ -30,15 +55,6 @@ def no_history():
 def no_errors():
     check_and_exit_if.no_folder(fst.ERRORS, 'errors', err.NO_ERRORS_FOLDER)
 
-def run_checks():
-    check_and_exit_if.bad_args(sys.argv)
-    check_and_exit_if.no_queue()
-    check_empty_queue()
-    check_and_exit_if.no_source_folder()
-    no_renames()
-    no_history()
-    no_errors()
-
 
 def dequeue():
     queue_files = files.ls(fst.QUEUE)
@@ -48,27 +64,41 @@ def dequeue():
     return sorted(queue_files, key=get_creation_time)[0] 
 
 
-def create_error_dict(name, message, command, data):
-    return {
-        erf.ERROR_FILE_NAME : name,
-        erf.ERROR_MESSAGE   : message,
-        erf.ERROR_COMMAND   : command,
-        erf.ERROR_DATA      : data
-    }
+def treat_all(joined_current_file, remaining, errors):
+    data = util.read_from_json(joined_current_file)
 
-def add_error(errors, name, message, command, data):
-    errors.append(
-        create_error_dict(name, message, command, data)
-    )
+    edits = data[trf.EDITS]
+    edit_all(edits, remaining, errors)
 
-def add_to_remaining(remaining, name):
-    remaining.append(name)
+    renames = data[trf.RENAMES]
+    rename_all(renames, remaining, errors)
+    
+    deletions = data[trf.DELETIONS]
+    delete_all(deletions, remaining, errors)
 
-def handle_error(errors, remaining, name, message, command, data):
-    add_error(errors, name, message, command, data)
-    add_to_remaining(remaining, name)
+def edit_all(edits, remaining, errors):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=cfg.NUM_PROCESSES) as executor:
+        results = [executor.submit(edit_one, edit) for edit in edits]
+        for future, edit in zip(concurrent.futures.as_completed(results), edits):
+            try:
+                future.result()
+            except Exception as e:
+                handle_error(errors, remaining, edit[trf.EDIT_ORIGINAL], str(e), trf.EDITS, edit)
+
+def edit_one(edit):
+    name = edit[trf.EDIT_ORIGINAL]
+    joined_src_path = files.get_joined_path(cfg.SOURCE, name)
+    joined_dst_path = files.get_joined_path(cfg.DESTINATION, edit[trf.EDIT_NAME])
+
+    times = edit[trf.EDIT_TIMES]
+    edit_video(joined_src_path, joined_dst_path, *times)
 
 
+def edit_video(joined_src_path, joined_dst_path, start, end=None):
+    if cfg.USE_MOVIEPY:
+        edit_moviepy(joined_src_path, joined_dst_path, start, end)
+    else:
+        edit_ffmpeg(joined_src_path, joined_dst_path, start, end)
 
 def edit_moviepy(joined_src_path, joined_dst_path, start, end=None):
     with mvp.VideoFileClip(joined_src_path) as file:
@@ -93,34 +123,25 @@ def edit_ffmpeg(joined_src_path, joined_dst_path, start, end=None):
     args.append(joined_dst_path)
     subprocess.run(args, check=True)
 
-def edit_video(joined_src_path, joined_dst_path, start, end=None):
-    if cfg.USE_MOVIEPY:
-        edit_moviepy(joined_src_path, joined_dst_path, start, end)
-    else:
-        edit_ffmpeg(joined_src_path, joined_dst_path, start, end)
+def handle_error(errors, remaining, name, message, command, data):
+    add_error(errors, name, message, command, data)
+    add_to_remaining(remaining, name)
 
-def edit_one(edit):
-    name = edit[trf.EDIT_ORIGINAL]
-    joined_src_path = files.get_joined_path(cfg.SOURCE, name)
-    joined_dst_path = files.get_joined_path(cfg.DESTINATION, edit[trf.EDIT_NAME])
+def add_error(errors, name, message, command, data):
+    errors.append(
+        create_error_dict(name, message, command, data)
+    )
 
-    times = edit[trf.EDIT_TIMES]
-    edit_video(joined_src_path, joined_dst_path, *times)
+def create_error_dict(name, message, command, data):
+    return {
+        erf.ERROR_FILE_NAME : name,
+        erf.ERROR_MESSAGE   : message,
+        erf.ERROR_COMMAND   : command,
+        erf.ERROR_DATA      : data
+    }
 
-def edit_all(edits, remaining, errors):
-    with concurrent.futures.ProcessPoolExecutor(max_workers=cfg.NUM_PROCESSES) as executor:
-        results = [executor.submit(edit_one, edit) for edit in edits]
-        for future, edit in zip(concurrent.futures.as_completed(results), edits):
-            try:
-                future.result()
-            except Exception as e:
-                handle_error(errors, remaining, edit[trf.EDIT_ORIGINAL], str(e), trf.EDITS, edit)
-
-def do_rename(src_name, dst_name):
-    joined_src_name = files.get_joined_path(cfg.SOURCE, src_name)
-    joined_dst_name = files.get_joined_path(cfg.RENAMES, dst_name)
-
-    os.rename(joined_src_name, joined_dst_name)
+def add_to_remaining(remaining, name):
+    remaining.append(name)
 
 def rename_all(renames, remaining, errors):
     for rename_source in renames:
@@ -130,10 +151,11 @@ def rename_all(renames, remaining, errors):
         except Exception as e:
             handle_error(errors, remaining, rename_source, str(e), trf.RENAMES, new_name)
 
-def do_delete(src_name):
+def do_rename(src_name, dst_name):
     joined_src_name = files.get_joined_path(cfg.SOURCE, src_name)
+    joined_dst_name = files.get_joined_path(cfg.RENAMES, dst_name)
 
-    os.remove(joined_src_name)
+    os.rename(joined_src_name, joined_dst_name)
 
 def delete_all(deletions, remaining, errors):
     for deletion_name in deletions:
@@ -142,23 +164,22 @@ def delete_all(deletions, remaining, errors):
         except Exception as e:
             handle_error(errors, remaining, deletion_name, str(e), trf.DELETIONS, None)
 
-def treat_all(joined_current_file, remaining, errors):
-    data = util.read_from_json(joined_current_file)
+def do_delete(src_name):
+    joined_src_name = files.get_joined_path(cfg.SOURCE, src_name)
 
-    edits = data[trf.EDITS]
-    edit_all(edits, remaining, errors)
-
-    renames = data[trf.RENAMES]
-    rename_all(renames, remaining, errors)
-    
-    deletions = data[trf.DELETIONS]
-    delete_all(deletions, remaining, errors)
+    os.remove(joined_src_name)
 
 
 def update_history(current_file, joined_current_file):
     joined_history_file = files.get_joined_path(fst.HISTORY, current_file)
     os.rename(joined_current_file, joined_history_file)
 
+
+def handle_errors(remaining, errors):
+    error_file_name = util.generate_timestamped_file_name()
+    write_errors(error_file_name, errors)
+    util.write_remaining(remaining)
+    exit_treatment_error(error_file_name)
 
 def write_errors(error_file_name, errors):
     joined_error_file_name = files.get_joined_path(fst.ERRORS, error_file_name)
@@ -172,26 +193,6 @@ def exit_treatment_error(error_file_name):
     util.print_error(f"one or more errors occurred during treatment logged in '{util.highlight(error_file_name)}'")
     sys.exit(err.TREATMENT_ERROR)
 
-def handle_errors(remaining, errors):
-    error_file_name = util.generate_timestamped_file_name()
-    write_errors(error_file_name, errors)
-    util.write_remaining(remaining)
-    exit_treatment_error(error_file_name)
-
-
-def main():
-    run_checks()
-
-    remaining, errors = util.load_remaining(), list()
-    current_file = dequeue()
-    joined_current_file = files.get_joined_path(fst.QUEUE, current_file)
-    treat_all(joined_current_file, remaining, errors)
-    update_history(current_file, joined_current_file)
-
-    if errors:
-        handle_errors(remaining, errors)
-
-    util.exit_success('successfully treated all files')
 
 if __name__ == '__main__':
     main()
