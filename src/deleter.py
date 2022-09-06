7ffd27cc0626eb956601e74c1d7bb2e82afbc97f
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-06 20:25:32 +1000

    fixing doouble newlines beneath imports

--- src/deleter.py
+++ src/deleter.py
@@ -6,15 +6,17 @@ import config as cfg
 import helpers.check_and_exit_if as check_and_exit_if
 import helpers.files as files
 
-def run_checks():
-    check_and_exit_if.bad_args(sys.argv)
-    check_and_exit_if.files_remaining()
-    check_and_exit_if.no_source_folder()
 
 def main():
     run_checks()
 
     files.do_folder_operation(cfg.SOURCE, shutil.rmtree)
+
+def run_checks():
+    check_and_exit_if.bad_args(sys.argv)
+    check_and_exit_if.files_remaining()
+    check_and_exit_if.no_source_folder()
+
 
 if __name__ == '__main__':
     main()
