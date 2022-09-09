7b7d11a5faaf3145e9a606745b2a298e60b73c85
Sunny Chen <z5267282@student.unsw.edu.au>
2022-09-09 16:44:06 +1000

    moving onto combining two edit functions into one

--- src/constants/treatment_format.py
+++ src/constants/treatment_format.py
@@ -1,9 +1,12 @@
 # keys within a treatment
-EDITS         = 'edits'
+EDITS            = 'edits'
 # keys within edits
-EDIT_ORIGINAL = 'original'
-EDIT_NAME     = 'new name'
-EDIT_TIMES    = 'times'
+EDIT_ORIGINAL    = 'original'
+EDIT_NAME        = 'new name'
+EDIT_TIMES       = 'times'
+# keys within edit times
+EDIT_TIMES_START = 'start'
+EDIT_TIMES_END   = 'end'
 
 RENAMES   = 'renames'
 DELETIONS = 'deletions'
