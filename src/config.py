709475841249ffc630407cc862473785ee85bba4
Sunny Windows <z5267282@student.unsw.edu>
2022-10-25 00:54:27 +1100

    batch 6 done

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # folder paths
-SOURCE      = ['D:', '\\', 'TF2 BIG', 'Team Fortress 2', '5']
+SOURCE      = ['D:', '\\', 'TF2 BIG', 'Team Fortress 2', '6']
 RENAMES     = ['D:', '\\', 'Videos', 'Renames']
-DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'TF2', '5']
+DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'TF2', '6']
 
 # multi threading and processing
 NUM_THREADS   = 4
