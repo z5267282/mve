45bb869b36698a4f14f91bd4aacb7c9a4d295df8
Sunny Chen <z5267282@student.unsw.edu.au>
2023-02-09 02:53:34 +1100

    fixed

--- fix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/dash
-
-grep -E -o '".*"' src/remaining.json | tr -d '"' | while read vid
-do
-    grep -F -q "$vid" -r history && continue
-    echo "$vid"
-done | sed -E -e 's/.*/"&"/' -e '$q' -e 's/$/,/'
-
