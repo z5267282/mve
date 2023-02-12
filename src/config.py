93c777c191bfdc9abe99ec1ea7f276be8941789d
Sunny Windows <z5267282@student.unsw.edu>
2023-02-13 02:34:38 +1100

    fixing config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # folder paths
 SOURCE      = ['D:', '\\', 'Videos', 'Minecraft']
-RENAMES     = ['D:', '\\', 'Videos', 'Edited Minecraft']
+RENAMES     = ['D:', '\\', 'Videos', 'Summer Minecraft Events']
 DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'Minecraft 23']
 
 # multi threading and processing
