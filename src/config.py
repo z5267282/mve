554da42dab38e9615bd3f54fa445ff4d8daf47f2
Sunny Windows <z5267282@student.unsw.edu>
2023-03-18 00:57:07 +1100

    dump of edits

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 # folder paths
-SOURCE      = ['D:', '\\', 'Videos', 'Team Fortress 2']
-RENAMES     = ['D:', '\\', 'Videos', 'Renames']
-DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'TF2']
+SOURCE      = ['D:', '\\', 'Videos', 'Minecraft']
+RENAMES     = ['D:', '\\', 'Videos', 'MC Renames']
+DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'Minecraft 23']
 
 # multi threading and processing
 NUM_THREADS   = 4
