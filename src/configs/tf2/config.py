004c729a0fd9416915f61e3dcb9c8d5448fcbec2
Sunny Windows <z5267282@student.unsw.edu>
2023-05-11 03:55:38 +1000

    desktop swapper

---
+++ src/configs/tf2/config.py
@@ -0,0 +1,17 @@
+# folder paths
+SOURCE      = ['D:', '\\', 'Videos', 'Team Fortress 2']
+RENAMES     = ['D:', '\\', 'Videos', 'Renames', 'TF2']
+DESTINATION = ['C:', '\\', 'Users', 'Sunny', 'Videos', 'Edits', 'TF2']
+
+# multi threading and processing
+NUM_THREADS   = 4
+NUM_PROCESSES = 4
+
+# MoviePy
+USE_MOVIEPY = False
+
+# testing
+TESTING = False
+
+# colours
+BOLD = False
