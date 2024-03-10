e6693f0bf8cdc3aff16ea75d32b841bb44c43bee
Sunny Chen <z5267282@student.unsw.edu.au>
2024-03-10 17:58:28 +1100

    attempt to run importlib.reload not working; original variables not changed

--- src/moment.py
+++ src/moment.py
@@ -1,5 +1,8 @@
-from json import dumps
+import json
+import importlib 
 import sys
+
+import config as cfg
 
 import constants.error as err
 import constants.json_settings as jsn
@@ -12,21 +15,34 @@ from viewer import run_loop, wrap_session
 def main():
     remaining, errors = gen_remaining(), []
     edits, renames, deletions = list(), dict(), list()
-    run_loop(edits, renames, deletions)
-    data = wrap_session(edits, renames, deletions)
-    treat_all(data, remaining, errors)
-    handle_errors(errors)
+    run_loop(remaining, edits, renames, deletions)
+    # data = wrap_session(edits, renames, deletions)
+    # treat_all(data, remaining, errors)
+    # handle_errors(errors)
     util.exit_treat_all_good()
 
 
 def gen_remaining():
-    source = input("enter source folder : ")
-    edits = input("enter ")
+    print("enter absolute paths for the following folders")
+    source = input("source : ")
+    edits = input("edits : ")
+    fix_config("test", "", "")
+    return []
+
+def fix_config(source, edits, renames):
+    cfg.SOURCE = source
+    cfg.DESTINATION = edits
+    cfg.RENAMES = renames
+    importlib.reload(cfg)
 
 
 def handle_errors(errors):
     if errors:
         util.print_error(
-            dumps(errors, indent=jsn.INDENT_SPACES)
+            json.dumps(errors, indent=jsn.INDENT_SPACES)
         )
         sys.exit(err.TREATMENT_ERROR)
+
+
+if __name__ == "__main__":
+    main()
