b0987b6de2fd88e9d870053b009c228a4fe6d8ce
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-22 01:32:05 +1000

    linter is disgusting it is going

--- src/config.py
+++ src/config.py
@@ -1,31 +1,49 @@
 from json import load
 from typing import Any
+import sys
 
+import constants.error as error
 import constants.file_structure as file_structure
+import constants.defaults as defaults
 
 import helpers.files as files
+import helpers.util as util
 
 
 def create_config():
+    def expect_paths_list(contents: dict[str, Any], key: str, code: int) -> str:
+        if key not in contents:
+            util.print_error(f"{contents} not in configuration file")
+            sys.exit(code)
+        return contents[key]
+
     class Configuration:
-        def __init__(self, source: dict[str, Any]) -> "Configuration":
+        def __init__(self, contents: dict[str, Any]) -> "Configuration":
             # folders
-            self.SOURCE: list[str] = source["SOURCE"]
-            self.RENAMES: list[str] = source["RENAMES"]
-            self.DESTINATION: list[str] = source["DESTINATION"]
+            self.SOURCE: list[str] = expect_paths_list(
+                contents, "SOURCE", error.CONFIG_MISSING_SOURCE)
+            self.RENAMES: list[str] = expect_paths_list(
+                contents, "RENAMES", error.CONFIG_MISSING_RENAMES
+            )
+            self.DESTINATION: list[str] = expect_paths_list(
+                contents, "DESTINATION", error.CONFIG_MISSING_DESTINATION
+            )
 
             # multi threading and processing
-            self.NUM_THREADS: int = source.get("NUM_THREADS", 4)
-            self.NUM_PROCESSES: int = source.get("NUM_PROCESSES", 4)
+            self.NUM_THREADS: int = contents.get(
+                "NUM_THREADS", defaults.NUM_THREADS)
+            self.NUM_PROCESSES: int = contents.get(
+                "NUM_PROCESSES", defaults.NUM_PROCESSES)
 
             # moviepy
-            self.USE_MOVIEPY: bool = source.get("USE_MOVIEPY", False)
+            self.USE_MOVIEPY: bool = contents.get(
+                "USE_MOVIEPY", defaults.USE_MOVIEPY)
 
             # testing
-            self.TESTING: bool = source.get("TESTING", False)
+            self.TESTING: bool = contents.get("TESTING", defaults.TESTING)
 
             # colours
-            self.BOLD: bool = source.get("BOLD", False)
+            self.BOLD: bool = contents.get("BOLD", defaults.BOLD)
 
     current_path: str = files.get_joined_path(
         file_structure.CONFIGS, "current.json")
@@ -44,10 +62,6 @@ def create_config():
     except FileNotFoundError:
         print(
             f"the current config does not have a corresponding file {config_path}")
-
-    for folder in ["SOURCE", "RENAMES", "DESTINATION"]:
-        if not files.folder_exists(folder):
-            print(f"{folder} does not exist")
 
     return Configuration(config)
 
---
+++ src/constants/defaults.py
@@ -0,0 +1,12 @@
+# multi threading and processing
+NUM_THREADS: int = 4
+NUM_PROCESSES: int = 4
+
+# moviepy
+USE_MOVIEPY: bool = False
+
+# testing
+TESTING: bool = False
+
+# colours
+BOLD: bool = False
