86710a75f0a8554d7a358cbd2005e64e6b803bc9
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-23 20:27:41 +1000

    deleter fixged

--- src/deleter.py
+++ src/deleter.py
@@ -1,7 +1,7 @@
 import shutil
 import sys
 
-import config as cfg
+import config
 
 import helpers.check_and_exit_if as check_and_exit_if
 import helpers.colours as colours
@@ -12,9 +12,12 @@ import helpers.util as util
 def main():
     run_checks()
 
-    files.do_folder_operation(cfg.SOURCE, shutil.rmtree)
+    TODO_FIX = "mac"
+    cfg = config.Config(TODO_FIX)
 
-    joined_path = files.get_joined_path(cfg.SOURCE, '')
+    files.do_folder_operation(cfg.source, shutil.rmtree)
+
+    joined_path = files.join_folder(cfg.source)
     util.exit_success(f"removed the folder '{colours.highlight(joined_path)}'")
 
 
