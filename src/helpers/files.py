b5dc9d767467fcc9457ddcbf2fc7ce1c80a42717
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-23 15:56:11 +1000

    files type hints

--- src/helpers/files.py
+++ src/helpers/files.py
@@ -1,21 +1,24 @@
 import os
+import typing
 
 
-def join_folder(paths_list):
+def join_folder(paths_list: list[str]) -> str:
     return os.path.join(*paths_list)
 
 
-def get_joined_path(paths_list, file_name):
+def get_joined_path(paths_list: list[str], file_name: str):
     return join_folder(paths_list + [file_name])
 
 
-def do_folder_operation(paths_list, handler):
+def do_folder_operation(
+    paths_list: list[str], handler: typing.Callable
+) -> typing.Any:
     return handler(
         join_folder(paths_list)
     )
 
 
-def ls(paths_list, recent=False):
+def ls(paths_list: list[str], recent=False) -> list[str]:
     return sorted(
         do_folder_operation(paths_list, os.listdir),
         key=lambda file_name: os.path.getctime(
@@ -25,5 +28,5 @@ def ls(paths_list, recent=False):
     )
 
 
-def folder_exists(paths_list):
+def folder_exists(paths_list: list[str]) -> bool:
     return do_folder_operation(paths_list, os.path.exists)
