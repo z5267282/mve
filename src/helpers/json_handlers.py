b670342394932c5b66b4d0f0304ace95d5b9054c
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-26 01:56:24 +1000

    remvoing jsn import

--- src/helpers/json_handlers.py
+++ src/helpers/json_handlers.py
@@ -1,11 +1,11 @@
 import json
 
-import constants.json_settings as jsn
+import constants.json_settings as json_settings
 
 
 def write_to_json(item, joined_path):
     with open(joined_path, 'w') as f:
-        json.dump(item, f, indent=jsn.INDENT_SPACES)
+        json.dump(item, f, indent=json_settings.INDENT_SPACES)
 
 
 def read_from_json(joined_path):
