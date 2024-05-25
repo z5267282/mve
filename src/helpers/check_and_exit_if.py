a1e57a156bb3357a8f291e815b0ef4692598d9d3
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-26 00:23:24 +1000

    removing argument checking

--- src/helpers/check_and_exit_if.py
+++ src/helpers/check_and_exit_if.py
@@ -8,9 +8,7 @@ import helpers.files as files
 import helpers.util as util
 
 
-# Command line args checking
-
-def bad_args(argv, length, usage_message):
+def bad_args(argv: list[str], length: int, usage_message: str = ''):
     program_name, args = argv[0], argv[1:]
 
     if len(args) == length:
@@ -26,20 +24,12 @@ def bad_args(argv, length, usage_message):
     sys.exit(err.BAD_COMMAND_LINE_ARGS)
 
 
-def no_args(argv):
-    bad_args(argv, 0, '')
-
-
-# File checking
-
 def no_file(joined_path, desc, code):
     if not os.path.exists(joined_path):
         util.stderr_print(
             f"the {desc} file '{colours.highlight(joined_path)}' does not exist")
         sys.exit(code)
 
-
-# Folder checking
 
 def no_folder(folder_paths: list[str], folder_desc: str, exit_code: int):
     if not files.folder_exists(folder_paths):
