9f8848def9a50f9c3e051af06afb65436f56534e
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-26 01:40:38 +1000

    adding options file

---
+++ src/constants/options.py
@@ -0,0 +1,22 @@
+'''String names for options that can be configured via a file-based config'''
+
+# folders
+SOURCE = "SOURCE"
+RENAMES = "RENAMES"
+DESTINATION = "DESTINATION"
+
+# file-order generation
+RECENT = "RECENT"
+
+# multiprocessing
+NUM_PROCESSES = "NUM_PROCESSES"
+
+# moviepy
+USE_MOVIEPY = "USE_MOVIEPY"
+MOVIEPY_THREADS = "MOVIEPY_THREADS"
+
+# testing
+TESTING = "TESTING"
+
+# colours
+BOLD = "BOLD"
