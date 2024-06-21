ded6524b34c69800ec7ca3bfa5e5a650e92d61a6
Sunny <sunny@pc>
2024-06-21 20:45:44 +1000

    generator hints fixed

--- src/generator.py
+++ src/generator.py
@@ -1,10 +1,10 @@
-'''Load all files from the given config's source folder and place them in a remaning JSON file.
-Only the base name is stored.
-If the source folder does not exist, the program terminates with status errr.NO_SOURCE_FOLDER.
-The generator checks if there are files yet to be treated and accordingly terminates with status error.FILES_REMAINING.
-If remaining JSON file does not exist, it is created.
-Files are stored from most recent to least recent.
-This behaviour can be toggled through the RECENT config flag.'''
+'''Load all files from the given config's source folder and place them in a
+remaning JSON file. Only the base name is stored. If the source folder does not
+exist, the program terminates with status error.NO_SOURCE_FOLDER. The generator
+checks if there are files yet to be treated and accordingly terminates with
+status error.FILES_REMAINING. If remaining JSON file does not exist, it is
+created. Files are stored from most recent to least recent. This behaviour can 
+be toggled through the RECENT config flag.'''
 
 import sys
 
