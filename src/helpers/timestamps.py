8a977e0d6a9a24b3a71b0e7438918ed714702b6c
Sunny <sunny@pc>
2024-06-21 21:00:36 +1000

    time handlers type hints

--- src/helpers/timestamps.py
+++ src/helpers/timestamps.py
@@ -1,10 +1,10 @@
 import datetime as dt
 
 
-def get_timestamp():
+def get_timestamp() -> str:
     right_now = dt.datetime.now()
     return right_now.strftime('%d.%m.%Y - %H%M')
 
 
-def generate_timestamped_file_name():
+def generate_timestamped_file_name() -> str:
     return f'{get_timestamp()}.json'
