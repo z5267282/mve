a81f46aa56755d3cb2cf99a850af2311355f42dd
Sunny <sunny@pc>
2024-06-21 20:58:09 +1000

    type hints for json handlers

--- src/helpers/json_handlers.py
+++ src/helpers/json_handlers.py
@@ -3,12 +3,12 @@ import json
 import constants.json_settings as json_settings
 
 
-def write_to_json(item, joined_path):
+def write_to_json(item: any, joined_path: str):
     with open(joined_path, 'w') as f:
         json.dump(item, f, indent=json_settings.INDENT_SPACES)
 
 
-def read_from_json(joined_path):
+def read_from_json(joined_path: str) -> any:
     with open(joined_path, 'r') as f:
         data = json.load(f)
     return data
