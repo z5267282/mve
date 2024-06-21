caf35312a0991892cc9cbff62e359f00a99604cc
Sunny <sunny@pc>
2024-06-21 21:01:21 +1000

    adding type hints for util

--- src/helpers/util.py
+++ src/helpers/util.py
@@ -7,17 +7,17 @@ import constants.colours as colours
 import helpers.colouring as colouring
 
 
-def stderr_print(message):
+def stderr_print(message: str):
     print(message, file=sys.stderr)
 
 
-def print_error(message):
+def print_error(message: str):
     stderr_print('{} {}'.format(
         colouring.colour_box(colours.RED, 'error'), message
     ))
 
 
-def print_success(message: str) -> None:
+def print_success(message: str):
     base_name = re.sub(
         r'\.py$', r'',
         os.path.basename(sys.argv[0])
@@ -36,7 +36,7 @@ def format_remaining(num_remaining: int) -> str:
     )
 
 
-def exit_success(message):
+def exit_success(message: str):
     print_success(message)
     sys.exit(0)
 
