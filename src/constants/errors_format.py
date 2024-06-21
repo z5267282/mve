00eb0237fb95d8ad2a954907b96a13d2653ddf4b
Sunny <sunny@pc>
2024-06-21 20:53:09 +1000

    adding type hints for errors formatting

--- src/constants/errors_format.py
+++ src/constants/errors_format.py
@@ -1,9 +1,9 @@
 # errors keys - keys for the a whole file in errors/
-ERRORS_VIDEOS = 'files'
+ERRORS_VIDEOS: str = 'files'
 # keys for a single dictionary inside the list of error videos
-ERROR_FILE_NAME = 'file name'
-ERROR_MESSAGE = 'message'
-ERROR_COMMAND = 'error'
-ERROR_DATA = 'data'
+ERROR_FILE_NAME: str = 'file name'
+ERROR_MESSAGE: str = 'message'
+ERROR_COMMAND: str = 'error'
+ERROR_DATA: str = 'data'
 
-ERRORS_PATHS = 'paths'
+ERRORS_PATHS: str = 'paths'
