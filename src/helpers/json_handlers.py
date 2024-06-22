6895a93455c48ee01a4394a0cc7256c2d992f02e
Sunny Chen <z5267282@student.unsw.edu.au>
2024-06-22 18:10:56 +1000

    json handlers type hints

--- src/helpers/json_handlers.py
+++ src/helpers/json_handlers.py
@@ -1,14 +1,15 @@
 import json
+import typing
 
 import constants.json_settings as json_settings
 
 
-def write_to_json(item: any, joined_path: str):
+def write_to_json(item: typing.Any, joined_path: str):
     with open(joined_path, 'w') as f:
         json.dump(item, f, indent=json_settings.INDENT_SPACES)
 
 
-def read_from_json(joined_path: str) -> any:
+def read_from_json(joined_path: str) -> typing.Any:
     with open(joined_path, 'r') as f:
         data = json.load(f)
     return data
