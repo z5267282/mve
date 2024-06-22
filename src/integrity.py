4972717d2be7f8081fd831e0350d068cf6f4cb51
Sunny Chen <z5267282@student.unsw.edu.au>
2024-06-23 01:59:16 +1000

    integrity should work

--- src/integrity.py
+++ src/integrity.py
@@ -1,6 +1,12 @@
+import os
 import sys
 
 import config
+
+import constants.colours as colours
+import constants.defaults as defaults
+import constants.environment as environment
+import constants.error as error
 
 import helpers.colouring as colouring
 import helpers.util as util
@@ -12,20 +18,41 @@ def main():
     # just match on the number of command line arguments
     match len(sys.argv[1:]):
         case 0:
-            pass
+            check_all_configs()
         case 1:
             check_one_config(sys.argv[1])
         case _:
             pass
-
     pass
+
+
+def check_all_configs():
+    '''Check the integrity of all configs from the MVE_CONFIGS environment
+    variable'''
+    configs_folder = os.getenv(environment.CONFIGS)
+    if configs_folder is None:
+        coloured_env_key = colouring.colour_format(
+            colours.RED, environment.CONFIGS, defaults.BOLD)
+        util.print_error(
+            f'the environment variable ${coloured_env_key} has not been set',
+            defaults.BOLD)
+        sys.exit(error.CONFIGS_ENVIRONMENT_NOT_SET)
+
+    # TODO: modurlaise this
+    configs = os.listdir(configs_folder)
+    plural = '' if len(configs) == 1 else 's'
+    print(
+        f'verifying the integrity of {len(configs)} config{plural} in {colouring.highlight(configs_folder, defaults.BOLD)}')
+    for i, config in enumerate(configs):
+        print(f'{i + 1}', end=' : ')
+        check_one_config(config)
 
 
 def check_one_config(name: str):
     state = config.Stateful(name)
     bold = state.cfg.bold
     util.print_success(
-        f'the integrity of config \'{colouring.highlight(name, bold)}\' has been verified',
+        f'the integrity of config \'{colouring.colour_format(colours.PURPLE, name, defaults.BOLD)}\' has been verified',
         bold)
 
 
