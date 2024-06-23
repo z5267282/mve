d7c9b355214245fb245b6e6aa73f37aeaf3b8975
Sunny <sunny@pc>
2024-06-23 15:12:35 +1000

    fixing modular support for loading environment variable

---
+++ src/helpers/load_env.py
@@ -0,0 +1,13 @@
+'''Load information from system environment variables'''
+
+import os
+import pathlib
+
+import constants.environment as environment
+
+
+def get_config_paths_from_environment() -> None | list[str]:
+    return list(pathlib.Path(raw).parts) \
+        if (raw := os.getenv(environment.CONFIGS)
+            ) is not None \
+        else raw
