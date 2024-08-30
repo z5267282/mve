f2d6601821635f00a2ee7920f449a48130191fa7
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-08-31 01:59:33 +1000

    working version of focus

---
+++ src/focus.py
@@ -0,0 +1,73 @@
+'''Edit one video on a loop.'''
+
+import json
+import os
+import pathlib
+import sys
+
+import config as config
+
+import constants.commands as commands
+import constants.defaults as defaults
+import constants.json_settings as json_settings
+
+import helpers.video_paths as video_paths
+
+import lib.edit as edit
+import lib.view as view
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print('enter a source file')
+        sys.exit(1)
+
+    # path information
+    source, = args
+    source_path = pathlib.Path(source)
+    dir_name: list[str] = list(source_path.parent.parts)
+    base_name: str = source_path.name
+    destination: list[str] = list(
+        pathlib.Path(os.path.join(os.path.expanduser('~'), 'Downloads')).parts
+    )
+
+    paths: video_paths.VideoPaths = video_paths.VideoPaths(
+        dir_name, destination, destination)
+
+    # edit information
+    cfg = config.Config(paths.source, paths.renames, paths.edits)
+    edits: list[dict] = []
+    errors: list[dict] = []
+    bold: bool = defaults.BOLD
+    while True:
+        # Can only enter editing commands (ie. s, m, e) and q
+        args: list[str] = input('focus.py : ').split(' ', 1)
+        command: str = args.pop(0)
+        raw_tokens: str = args.pop() if args else ''
+        match command:
+            case commands.QUIT:
+                break
+            case commands.HELP:
+                print('enter one of [sme]')
+            case commands.END:
+                view.do_end(base_name, raw_tokens, edits, paths, bold)
+            case commands.START:
+                view.do_start(base_name, raw_tokens, edits, paths, bold)
+            case commands.MIDDLE:
+                view.do_middle(base_name, raw_tokens, edits, paths, bold)
+
+    # Queue stored in memory, not written to disk
+    # Then when you quit, edits are performed.
+    data = view.wrap_session(edits, {}, [])
+    edit.treat_all(data, cfg.use_moviepy, cfg.moviepy_threads,
+                   cfg.num_processes, [], errors, paths)
+    if errors:
+        print('ignoring these files due to errors')
+        print(json.dumps(errors, indent=json_settings.INDENT_SPACES))
+
+    print('focus.py complete')
+
+
+if __name__ == '__main__':
+    main()
