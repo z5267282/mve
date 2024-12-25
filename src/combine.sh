95a5d314c3599a029cd7647144ce47a6c5467ef8
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-12-25 21:16:00 +1000

    combine mode: deleting ffmpeg combine script

--- src/combine.sh
+++
@@ -1,3 +0,0 @@
-ls -1 -tr hsieh/* | sed -E 's/^/file /' > clips.txt
-# taken from: https://medium.com/@3valuedlogic/how-to-concatenate-video-clips-with-ffmpeg-db77ac797913
-ffmpeg -f concat -i clips.txt ffmpeg.mp4
