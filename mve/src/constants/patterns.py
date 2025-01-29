bf2518290bbefdfbe94a9e97aa6d0e95fffef847
z5267282 <80458779+z5267282@users.noreply.github.com>
2025-01-29 13:08:45 +1100

    need to escape hyphen in regex

--- mve/src/constants/patterns.py
+++ mve/src/constants/patterns.py
@@ -6,7 +6,7 @@ CONFIG_NAME: Pattern = Pattern(
 INTEGER_SECONDS: Pattern = Pattern(r'-?[0-9]+', 'integer number of seconds')
 LEADING_NUMBER: Pattern = Pattern(r'[0-9]+', 'leading number')
 TREATED_FILE_NAME: Pattern = Pattern(
-    r'[a-zA-Z0-9 .-_]+', "alphanumeric characters, ' ', '.', '-', or '_'")
+    r'[a-zA-Z0-9 .\-_]+', "alphanumeric characters, ' ', '.', '-', or '_'")
 TIMESTAMP: Pattern = Pattern(
     fr'([0-5]?[0-9]{SHORT_HAND})?[0-5]?[0-9]{SHORT_HAND}[0-5]?[0-9]',
     f'timestamp in form <[hour]{SHORT_HAND}min{SHORT_HAND}sec>')
